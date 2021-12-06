5f19b5f75458f859742930f0ac2a264049dacb0e
Albert Marashi <albert@framework.tools>
2021-12-06 19:42:16 +1030

    Working, but iterator broken

--- src/main.rs
+++ src/main.rs
@@ -4,64 +4,71 @@
     panic_info_message,
     asm,
     // llvm_asm,
-    // global_asm
+    global_asm
 )]
+
+global_asm!(include_str!("boot.s"));
 
 // ///////////////////////////////////
 // / RUST MACROS
 // ///////////////////////////////////
-#[macro_export]
-macro_rules! print
-{
-	($($args:tt)+) => ({
-        let uart_data = 0x1000_0000 as *mut u8;
-        for c in b"Hello, world!\n" {
-            unsafe { uart_data.write_volatile(*c) };
-        }
-		// let _ = write!(crate::uart::Uart::new(0x1000_0000), $($args)+);
-	});
-}
-#[macro_export]
-macro_rules! println
-{
-	() => ({
-		print!("\r\n")
-	});
-	($fmt:expr) => ({
-		print!(concat!($fmt, "\r\n"))
-	});
-	($fmt:expr, $($args:tt)+) => ({
-		print!(concat!($fmt, "\r\n"), $($args)+)
-	});
-}
+// #[macro_export]
+// macro_rules! print
+// {
+// 	($($args:tt)+) => ({
+//         let uart_data = 0x1000_0000 as *mut u8;
+//         for c in b"Hello, world!\n" {
+//             unsafe { uart_data.write_volatile(*c) };
+//         }
+// 		// let _ = write!(crate::uart::Uart::new(0x1000_0000), $($args)+);
+// 	});
+// }
+// #[macro_export]
+// macro_rules! println
+// {
+// 	() => ({
+// 		print!("\r\n")
+// 	});
+// 	($fmt:expr) => ({
+// 		print!(concat!($fmt, "\r\n"))
+// 	});
+// 	($fmt:expr, $($args:tt)+) => ({
+// 		print!(concat!($fmt, "\r\n"), $($args)+)
+// 	});
+// }
 
-// ///////////////////////////////////
-// / LANGUAGE STRUCTURES / FUNCTIONS
-// ///////////////////////////////////
-#[no_mangle]
-extern "C" fn eh_personality() {}
+// // ///////////////////////////////////
+// // / LANGUAGE STRUCTURES / FUNCTIONS
+// // ///////////////////////////////////
+// #[no_mangle]
+// extern "C" fn eh_personality() {}
+// #[panic_handler]
+// fn panic(info: &core::panic::PanicInfo) -> ! {
+//     print!("Aborting: ");
+//     if let Some(p) = info.location() {
+//         println!(
+//             "line {}, file {}: {}",
+//             p.line(),
+//             p.file(),
+//             info.message().unwrap()
+//         );
+//     } else {
+//         println!("no information available.");
+//     }
+//     abort();
+// }
+// #[no_mangle]
+// extern "C" fn abort() -> ! {
+//     loop {
+//         unsafe {
+//             asm!("wfi");
+//         }
+//     }
+// }
+
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    print!("Aborting: ");
-    if let Some(p) = info.location() {
-        println!(
-            "line {}, file {}: {}",
-            p.line(),
-            p.file(),
-            info.message().unwrap()
-        );
-    } else {
-        println!("no information available.");
-    }
-    abort();
-}
-#[no_mangle]
-extern "C" fn abort() -> ! {
-    loop {
-        unsafe {
-            asm!("wfi");
-        }
-    }
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    hcf();
 }
 
 // ///////////////////////////////////
@@ -74,11 +81,23 @@ extern "C" fn abort() -> ! {
 #[no_mangle]
 extern "C" fn kmain() -> ! {
     let uart_data = 0x1000_0000 as *mut u8;
-    for c in b"Hello, world!\n" {
-        unsafe { uart_data.write_volatile(*c) };
+
+
+    for c in b"Hello, world!" {
+        unsafe {
+            uart_data.write_volatile(*c as u8);
+        };
     }
 
     hcf();
+    // unsafe {
+    //     asm!("
+    //         li t1, 0x10000000
+    //         li t0, 'a'; sb t0, (t1)
+    //     ");
+    //     uart_data.write_volatile('x' as u8);
+    // };
+    // hcf();
 
     // Main should initialize all sub-systems and get
     // ready to start scheduling. The last thing this
